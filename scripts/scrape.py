2d973c3b0d9345bcb48ff6fb59bb2de214f2272a
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-10-14 16:01:33 -0400

    mainly research

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -7,7 +7,14 @@ HEADERS = {"User-Agent": "Mozilla/5.0"}
 
 
 def get_Bob_Dylan_soup():
-    response = requests.get(BASE_URL + "/wiki/Bob_Dylan", headers=HEADERS)
+    response = requests.get(
+        BASE_URL + "/wiki/List_of_awards_and_nominations_received_by_Bob_Dylan",
+        headers=HEADERS,
+    )
     return BeautifulSoup(response.content, "lxml")
 
+
 soup = get_Bob_Dylan_soup()
+soup_find = soup.find("table", {"class": "wikitable"})
+soup_select = soup.select("table.wikitable")
+print(soup_select)
