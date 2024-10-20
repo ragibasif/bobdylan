f8b7b155bf40c83f9f539175c22c08e1ae4641a5
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-10-20 13:38:26 -0400

    add: main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import sys
+import pandas as pd
+
+DATA_FILE = "./dataset/lyrics.csv"
+
+
+class Data:
+    def __init__(self, name="Bob Dylan"):
+        self.name = name
+        self.data = pd.read_csv(DATA_FILE)
+        if self.data is None:
+            print("Could not load data file {}".format(DATA_FILE), file=sys.stderr)
+            sys.exit(1)
+
+        self.data_info = self.data.info()
+        self.data_described = self.data.describe()
+        # print(self.data_info)
+        # print(self.data_describe)
+
+    def album(self):
+        album_song_counts = self.data.groupby("album")["title"].count()
+        album_total = (
+            self.data["album"].value_counts().shape[0]
+        )  # == 39 (data does not include 'Shadow Kingdom')
+        print(album_song_counts)
+        print("Album total: ", album_total)
+
+    def year(self):
+        yearly_song_counts = self.data.groupby("release_year")["title"].count()
+        print("yearly_song_counts:\n", yearly_song_counts)
+
+    def song(self):
+        song_total = (
+            self.data["title"].value_counts().shape[0]
+        )  # 39 (data does not include 'Shadow Kingdom')
+        print("Song total: ", song_total)
+
+    def lyrics(self):
+        all_lyrics = self.data["lyrics"]
+        print(all_lyrics)
+
+
+def main():
+    app = Data()
+    app.album()
+    app.song()
+    app.year()
+    app.lyrics()
+
+
+if __name__ == "__main__":
+    main()
--- scripts/references.md
+++
@@ -1,3 +0,0 @@
-1. https://en.wikipedia.org/wiki/Bob_Dylan
-2. https://en.wikipedia.org/wiki/List_of_awards_and_nominations_received_by_Bob_Dylan
-3. https://www.kaggle.com/datasets/cloudy17/bob-dylan-songs
